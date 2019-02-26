facb796d379b0f56293e70a41706383225ddd9f0
segalzeyalz <segaleseo@gmail.com>
2019-02-26 08:03:01 +0200

    use a computed

--- methods/script.js
+++ methods/script.js
@@ -2,11 +2,18 @@ new Vue({
     el: '#app',
     data: {
         num:0,
-        range: 100
+        range: 100,
+        firstName: "",
+        lastName:""
     },
     methods: {
         randomizeNum: function(){
             this.num = Math.floor(Math.random()*this.range)
         }
-  }
+  }, 
+  computed: {
+    fullName: function(){
+        return `${this.firstName} ${this.lastName}`
+    }
+  },
 })
